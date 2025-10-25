70cedba858e065f84a4da738b8381c77e9e38dbf
Dmitriy <dmitrii_shevel94@mail.ru>
2025-10-25 21:13:10 +0500

    Удалил ненужную страницу

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.green,
-        scaffoldBackgroundColor: Colors.grey[200],
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.blueAccent,
+            brightness: Brightness.light
+        ),
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          )
+        ),
       ),
       home: HomePage(),
     );
@@ -24,6 +31,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Home Page'),
       ),
       body: Center(
